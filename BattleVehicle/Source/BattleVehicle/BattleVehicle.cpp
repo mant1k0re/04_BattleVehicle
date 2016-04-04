af64ecbf9b8c999ba1d2c8e8f7844603d4bf4484
Raphaël Honoré <raphael.honore@gmail.com>
2016-04-04 07:45:06 +0200

    Battle tank 01 - adding and deleting landscape

---
+++ BattleVehicle/Source/BattleVehicle/BattleVehicle.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleVehicle.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BattleVehicle, "BattleVehicle" );
